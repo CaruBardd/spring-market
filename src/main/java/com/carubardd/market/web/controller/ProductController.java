2763d72cbd96b1d5f08dd04fba4ca53e0de7621b
CaruBardd <diego35andres199@gmail.com>
2022-04-15 22:39:34 -0500

    Exposicion de API

--- src/main/java/com/carubardd/market/web/controller/ProductController.java
+++ src/main/java/com/carubardd/market/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.carubardd.market.web.controller;
 import com.carubardd.market.domain.Product;
 import com.carubardd.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,23 +15,30 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping("/show/all")
     public List<Product> getAll() {
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId) {
+    @GetMapping("/show/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId) {
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId) {
+    @GetMapping("/show/category/{id}")
+    public Optional<List<Product>> getByCategory(@PathVariable("id") int categoryId) {
         return productService.getByCategory(categoryId);
     }
 
-    public Product save (Product product) {
+    @PostMapping("/new")
+    public Product save (
+            @RequestBody Product product
+    ) {
         return productService.save(product);
     }
 
-    public boolean delete (int productId) {
+    @DeleteMapping("/delete/{id}")
+    public boolean delete (@PathVariable("id") int productId) {
         return productService.delete(productId);
     }
 
